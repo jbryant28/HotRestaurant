0a2464073a66f142d4b2e0cfafe6ffbdcddda586
JohnB <jbryantmatthews@gmail.com>
2019-01-31 15:43:54 -0500

    saving

--- Solved/db/connection.js
+++ Solved/db/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "qwertasdfg1",
     database: "restaurant_db"
   });
 }
